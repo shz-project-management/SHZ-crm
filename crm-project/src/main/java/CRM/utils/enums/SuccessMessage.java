5c4bd384419e8974c7e4e8e042be23864bc5bd29
ZivHausler <zivhousler@gmail.com>
2022-12-17 16:20:10 +0200

    Added user DTO and some success messages.

--- crm-project/src/main/java/CRM/utils/enums/SuccessMessage.java
+++ crm-project/src/main/java/CRM/utils/enums/SuccessMessage.java
@@ -4,8 +4,11 @@ package CRM.utils.enums;
  * enum class with Success Messages to send more accurate messages.
  */
 public enum SuccessMessage {
+    LOGIN("Successfully logged in to the system!"),
+    REGISTER("The user has been successfully registered to the database"),
     DELETED("The board has been deleted"),
     FOUND("Successfully fetched this element in the database");
+
 
     private final String message;
     private SuccessMessage(final String message) {
