f9aa79425efddafa7082cfd667c97f2e47809adc
ZivHausler <zivhousler@gmail.com>
2022-12-19 22:34:57 +0200

    finished delete item and commends inside it

--- crm-project/src/main/java/CRM/repository/CommentRepository.java
+++ crm-project/src/main/java/CRM/repository/CommentRepository.java
@@ -1,10 +1,15 @@
 package CRM.repository;
 
 import CRM.entity.Comment;
+import CRM.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+    @Transactional
+    @Modifying
+    void deleteAllByParentItem(Item item);
 }
--- crm-project/src/main/java/CRM/repository/ItemRepository.java
+++ crm-project/src/main/java/CRM/repository/ItemRepository.java
@@ -11,5 +11,4 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findAllByBoard(Board board);
-
 }
