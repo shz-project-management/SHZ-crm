9ef2cffdcbfb317c3ee94f73ea2f8385aa69c7e1
hadi6645 <hadi6645@gmail.com>
2022-12-20 11:13:53 +0200

    changed class display name

--- crm-project/src/test/java/CRM/service/StatusServiceTest.java
+++ crm-project/src/test/java/CRM/service/StatusServiceTest.java
@@ -24,7 +24,7 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@DisplayName("StatusService create method tests")
+@DisplayName("StatusService tests")
 class StatusServiceTest {
 
     @Mock
