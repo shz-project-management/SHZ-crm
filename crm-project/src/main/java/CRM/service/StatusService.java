d2430e14a2f3a405b9bbb751f55eb2cd3ecdb88f
ShaiLevi23 <shailevi23@gmail.com>
2022-12-22 13:00:10 +0200

    Added documentation before changing architecture

--- crm-project/src/main/java/CRM/service/StatusService.java
+++ crm-project/src/main/java/CRM/service/StatusService.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
-public class StatusService implements AttributeService {
+public class StatusService {
 
     @Autowired
     private StatusRepository statusRepository;
@@ -25,7 +25,6 @@ public class StatusService implements AttributeService {
      * @param status The status object to be persisted.
      * @return The persisted status object.
      */
-    @Override
     public Status create(Attribute status) { // also get the board ID and the type(class) of attribute I want to create
         // check if board even exists
 
@@ -44,7 +43,6 @@ public class StatusService implements AttributeService {
      * Deletes the given status from the repository.
      * @param statusId the status ID to delete
      */
-    @Override
     public boolean delete(long statusId) {
         // get the board Id and make sure it exsits
 
@@ -65,7 +63,6 @@ public class StatusService implements AttributeService {
      * @throws IllegalArgumentException if the specified id is invalid.
      * @throws NullPointerException     if the specified id is null.
      */
-    @Override
     public Status get(long id) {
         // also get the boardId, and find it in the database
 
@@ -73,22 +70,31 @@ public class StatusService implements AttributeService {
         return Validations.doesIdExists(id, statusRepository);
     }
 
-    @Override
+
     public Status update(UpdateObjectRequest statusRequest, long statusId) throws NoSuchFieldException {
+        //get in method signature the board id, get the attributeRequest and attributeId and Class clz (type,section,status)
+        //find the board in db
+        //check if attribute exists, if it exists get the attribute and update it
+        //use update method in the board entity
+
         Status status = Validations.doesIdExists(statusId, statusRepository);
         Validations.setContentToFieldIfFieldExists(status, statusRequest.getFieldName(), statusRequest.getContent());
+
         return statusRepository.save(status);
     }
 
-    /**
-     * This method is used to retrieve all the statuses.
-     *
-     * @return A list containing all the statuses.
-     */
-    @Override
-    public List<Status> getAll() {
-        return statusRepository.findAll();
-    }
+//    /**
+//     * This method is used to retrieve all the statuses.
+//     *
+//     * @return A list containing all the statuses.
+//     */
+//    @Override
+//    public List<Status> getAll() {
+//        get Class clz and boardId in method signature
+//        findAll method in board entity.
+//
+//        return statusRepository.findAll();
+//    }
 
     /**
      * This method is used to retrieve all the statuses that belong to the board with the specified id.
@@ -99,8 +105,9 @@ public class StatusService implements AttributeService {
      * @throws IllegalArgumentException if the specified board id is invalid.
      * @throws NullPointerException     if the specified board id is null.
      */
-    @Override
-    public List<Status> getAllInBoard(long boardId) {
+    public List<Status> getAllAttributesInBoard(long boardId) {
+        //get Class clz and boardId in method signature
+        //getAllAttributeInBoard method in board entity.
         Board board = Validations.doesIdExists(boardId, boardRepository);
         return statusRepository.findByBoard(board);
     }
