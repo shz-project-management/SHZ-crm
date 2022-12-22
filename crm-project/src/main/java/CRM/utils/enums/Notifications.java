67e1d1a3cab0d93657f9e967cea614a7be5c8fba
hadi6645 <hadi6645@gmail.com>
2022-12-22 17:48:01 +0200

    implemented the initiate of the notifications in the db

--- crm-project/src/main/java/CRM/utils/enums/Notifications.java
+++ crm-project/src/main/java/CRM/utils/enums/Notifications.java
@@ -4,10 +4,18 @@ package CRM.utils.enums;
  * enum class that creates different notifications
  */
 public enum Notifications {
-    ASSIGNED_TO_ME,
-    STATUS_CHANGED,
-    COMMENT_ADDED,
-    ITEM_DELETED,
-    ITEM_DATA_CHANGED,
-    USER_ADDED;
+    ASSIGNED_TO_ME("Items assigned to me", "One or multiple items were assigned to me"),
+    STATUS_CHANGED("Items status changed", "Item that is related to me has it status changed"),
+    COMMENT_ADDED("Items comment added", "Item that is related to me has a new comment"),
+    ITEM_DELETED("Items deleted", "Item that is related to me has been deleted"),
+    ITEM_DATA_CHANGED("Items data changed", "Item that is related to me has had a change in some data"),
+    USER_ADDED("User added to the system", "A new user has been added to this board");
+
+    public final String name;
+    public final String description;
+
+    private Notifications(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
