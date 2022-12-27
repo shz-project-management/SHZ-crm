0cf57d90df7131bbf1ae2c77541a6df3bdb82fb6
Ziv Hausler <71410667+ZivHausler@users.noreply.github.com>
2022-12-27 12:17:52 +0200

    Update Notifications.java

--- crm-project/src/main/java/CRM/utils/enums/Notifications.java
+++ crm-project/src/main/java/CRM/utils/enums/Notifications.java
@@ -7,12 +7,12 @@ import lombok.Getter;
  */
 @Getter
 public enum Notifications {
-    ASSIGNED_TO_ME("One or multiple items has been assigned to me"),
+    ASSIGNED_TO_ME("Item has been assigned to me"),
     STATUS_CHANGED("Item that is related to me had it's status changed"),
     COMMENT_ADDED("Item that is related to me has a new comment"),
     ITEM_DELETED("Item that is related to me has been deleted"),
     ITEM_DATA_CHANGED("Item that is related to me had some data change"),
-    USER_ADDED("A new user has been added to a board i am in");
+    USER_ADDED("A new user has been added to a board I am taking part in");
 
     public final String name;
 
